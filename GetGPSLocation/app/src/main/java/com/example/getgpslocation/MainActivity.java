1d06476bcae92154c067ff9bd17bf2daf019af26
BrandonWalker <brandnwalker024@gmail.com>
2019-01-08 14:27:45 -0700

    Add files via upload

---
+++ GetGPSLocation/app/src/main/java/com/example/getgpslocation/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.getgpslocation;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class amHERE extends ActionBarActivity {
+
+	Button btnShowLocation;
+	
+	GPSTracker gps;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        btnShowLocation = (Button) findViewById(R.id.show_location);
+        
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				gps = new GPSTracker(MainActivity.this);
+
+					if(gps.canGetLocation()) {
+						double latitude;
+						double longitude;
+						latitude = Double.toString(location.getLatitude());
+						longitude= Double.toString(location.getLongitude())
+						Toast.makeText(
+								getApplicationContext(),
+								"Your Location is -\nLat: " + latitude + "\nLong: "
+										+ longitude, Toast.LENGTH_LONG).show();
+					} else {
+						gps.showSettingsAlert();
+				}
+			}
+		});
+    }
+}
